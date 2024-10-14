b2943be862f8fc9529e1890e3829a623aa449ebc
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-15 04:41:54 +0500

    Two Sums using 2 loops

---
+++ LeetCode/twoSumByLoops.js
@@ -0,0 +1,10 @@
+const twoSum = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([2, 11, 7, 15], 9));
