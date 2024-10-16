cafafe16cacef3e0b874039af38dada0f0b3c57a
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-17 01:10:18 +0500

    Implemented Enqueue

--- LeetCode/Queue.js
+++ LeetCode/Queue.js
@@ -7,7 +7,24 @@ class Node {
 class Queue {
   constructor(val) {
     this.front = new Node(val);
-    this.rear = front;
+    this.rear = this.front;
     this.length = 1;
   }
+  enqueue(val) {
+    const node = new Node(val);
+    if (!this.front) {
+      this.front = node;
+      this.read = node;
+      return this;
+    }
+    this.rear.next = node;
+    this.rear = node;
+    this.length++;
+    return this;
+  }
 }
+
+let queue = new Queue(2);
+queue.enqueue(1);
+queue.enqueue(6);
+console.log(queue);
