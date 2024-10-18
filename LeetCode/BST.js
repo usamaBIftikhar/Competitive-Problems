be1c8eaf2388164889205279bfbe727596b689b0
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-18 05:28:52 +0500

    created BST

---
+++ LeetCode/BST.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BST {
+  constructor(val) {
+    this.root = null;
+  }
+}
+const tree = new BST();
+console.log(tree);
