86b6b805b606c68e245506661682af1aafbdd176
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-18 05:43:15 +0500

    Added insert method in BST

--- LeetCode/BST.js
+++ LeetCode/BST.js
@@ -9,6 +9,37 @@ class BST {
   constructor(val) {
     this.root = null;
   }
+  insert(val) {
+    const node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let cur = this.root;
+    while (true) {
+      if (node.data === cur.data) {
+        return undefined;
+      }
+      if (node.data > cur.data) {
+        if (cur.right == null) {
+          cur.right = node;
+          return this;
+        }
+        cur = cur.right;
+      } else {
+        if (cur.left == null) {
+          cur.left = node;
+          return this;
+        }
+        cur = cur.left;
+      }
+    }
+  }
 }
 const tree = new BST();
+tree.insert(8);
+tree.insert(5);
+tree.insert(12);
+tree.insert(4);
+tree.insert(10);
 console.log(tree);
