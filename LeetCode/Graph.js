6531ddbc67f32ec98fa50ec7d60bf8b9ea8a1d4e
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-19 01:26:22 +0500

    Implemented add vertex method

--- LeetCode/Graph.js
+++ LeetCode/Graph.js
@@ -2,7 +2,17 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+  addVertex(vrtx) {
+    if (!this.adjacencyList[vrtx]) {
+      this.adjacencyList[vrtx] = [];
+    }
+    return false;
+  }
 }
 
 let graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
 console.log(graph);
