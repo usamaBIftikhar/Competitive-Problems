dd728f7d944d7d8334dd1c2889d955b8febc721c
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-20 21:25:00 +0500

    Minor changes

--- LeetCode/Graph.js
+++ LeetCode/Graph.js
@@ -16,12 +16,6 @@ class Graph {
     }
     return false;
   }
-  removeEdge(vrt1, vrt2) {
-    if (this.adjacencyList[vrt1] && this.adjacencyList[vrt2]) {
-      return true;
-    }
-    return false;
-  }
 }
 
 let graph = new Graph();
