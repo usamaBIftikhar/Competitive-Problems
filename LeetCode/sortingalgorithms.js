9dbc1726e5713dae3fdd6c912421942b20bed2b0
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-23 04:04:22 +0500

    Implemented selection sort

--- LeetCode/sortingalgorithms.js
+++ LeetCode/sortingalgorithms.js
@@ -10,5 +10,20 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
-
-console.log(bubbleSort([4, 5, 6, 3, 2]));
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (i !== minIndex) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+};
+console.log(selectionSort([4, 5, 6, 3, 2]));
