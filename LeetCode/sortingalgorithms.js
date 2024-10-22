64de34200f8fafc9429ce444b1609ad9d1c13994
Usama Bin Iftikhar <usamabiniftikhar1@gmail.com>
2024-10-23 03:57:08 +0500

    implemented bubble sort

---
+++ LeetCode/sortingalgorithms.js
@@ -0,0 +1,14 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([4, 5, 6, 3, 2]));
